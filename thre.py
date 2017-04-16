aa49f3b0275eb6697d0ae1c2a58e4a790bea80b3
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-16 22:31:13 +0600

    List

---
+++ thre.py
@@ -0,0 +1,12 @@
+names = ["Dima", "Gosha", "Oleg", "Vasiliy"]
+print(names[0])
+print(names[1])
+print(names[-1])
+print(names[-2])
+message = names[0].lower() + " " + "your email"
+print(message)
+
+motobikes = ["honda", "suzuki", "ktm", "yamaha"]
+dream = "I want " + "motobike " + motobikes[1].capitalize()
+print(dream.title())
+
