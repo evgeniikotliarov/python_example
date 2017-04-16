65b2777ed6878a38f1aa9059f079990110bb752a
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-16 23:32:16 +0600

    Методы с листом

--- thre.py
+++ thre.py
@@ -7,6 +7,21 @@ message = names[0].lower() + " " + "your email"
 print(message)
 
 motobikes = ["honda", "suzuki", "ktm", "yamaha"]
-dream = "I want " + "motobike " + motobikes[1].capitalize()
+a = "ducatti"
+motobikes.append(a)
+dream = "I want " + "motobike " + motobikes[-1].capitalize()
 print(dream.title())
+print(motobikes)
 
+max = "Maxim"
+omka = "Omurbek"
+names.append(max)
+names.append(omka)
+print(names)
+del names[-2]
+poped_names = names.pop(1)
+names.remove("Dima")
+message = "Studens "
+print(message, names)
+print(poped_names)
+print(names)
