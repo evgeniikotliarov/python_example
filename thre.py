ed8615c4ae9c56513d79ae261bc5b7ad73308811
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-17 22:55:15 +0600

    list

--- thre.py
+++ thre.py
@@ -15,6 +15,7 @@ print(motobikes)
 
 max = "Maxim"
 omka = "Omurbek"
+misha = "Mikhail"
 names.append(max)
 names.append(omka)
 print(names)
@@ -24,4 +25,17 @@ names.remove("Dima")
 message = "Studens "
 print(message, names)
 print(poped_names)
+print(names)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+names.insert(1, misha)
+print(names)
+two_guys = names.pop(0)
+print(names)
+message_2 = "Don't come to " + two_guys + ", I'm sorry"
+message_3 = "Don't come to " + names.pop(2) + ", I'm sorry"
+print(message_2)
+print(message_3.title())
+print(names)
+del names[0]
+del names[0]
 print(names)
