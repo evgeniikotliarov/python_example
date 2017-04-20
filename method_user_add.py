ff8cba6cc79d5c722d33314afd7a44dc78582d7b
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-20 23:39:33 +0600

    Iterators

---
+++ method_user_add.py
@@ -0,0 +1,8 @@
+pizza = ['pipperoni', 'chilli', 'mozarella']
+print(pizza)
+for piz in pizza:
+    print(piz + " I like it")
+
+for pis in pizza:
+    print("Do you like " + pis.title() + ".\n")
+print("Bla bla bla")
