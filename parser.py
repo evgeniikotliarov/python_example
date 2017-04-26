292237f33498427958cf50507925e001108447a6
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-04-26 23:38:30 +0600

    Clean

--- parser.py
+++
@@ -1,7 +0,0 @@
-string = 'name=vasya&password=123456&age=120'
-string_1 = string.split('&')
-data = {}
-for entry in string_1:
-    key, value = entry.split("=")
-    data[key] = value
-
--- thre.py
+++ thre.py
@@ -31,35 +31,3 @@ for value in eat:
 eat = ('pizza', 'sushi', 'fanta', 'coffee', 'mozzarella')
 for value in eat:
     print(value)
-# a = "ducatti"
-# motobikes.append(a)
-# dream = "I want " + "motobike " + motobikes[-1].capitalize()
-# print(dream.title())
-# print(motobikes)
-#
-# max = "Maxim"
-# omka = "Omurbek"
-# misha = "Mikhail"
-# names.append(max)
-# names.append(omka)
-# print(names)
-# del names[-2]
-# poped_names = names.pop(1)
-# names.remove("Dima")
-# message = "Studens "
-# print(message, names)
-# print(poped_names)
-# print(names)
-# print("++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-# names.insert(1, misha)
-# print(names)
-# two_guys = names.pop(0)
-# print(names)
-# message_2 = "Don't come to " + two_guys + ", I'm sorry"
-# message_3 = "Don't come to " + names.pop(2) + ", I'm sorry"
-# print(message_2)
-# print(message_3.title())
-# print(names)
-# del names[0]
-# del names[0]
-# print(names)
