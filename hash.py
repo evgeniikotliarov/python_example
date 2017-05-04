4f20020ed35b1bfcc27546f7f92c129754a59abb
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-04 22:21:19 +0600

    Hash

---
+++ hash.py
@@ -0,0 +1,36 @@
+# man = {'first_name': 'Dima', 'last_name': 'Kinder', 'age': '30', 'city':'Kant'}
+# print(man['last_name'], man['first_name'])
+# numbers = {'Dima':5, 'Leha':7, 'Alex':9}
+# print(numbers["Leha"])
+
+# favorite_language = {
+#     'Matz': 'ruby',
+#     'Jen': 'python',
+#     'Sarah': 'c',
+#     'Joshua': 'java'
+# }
+# print("We love " + favorite_language['Matz'].upper() + " very" "\n" "and " + favorite_language['Joshua'].title() + " very nice")
+
+# for k,v in favorite_language.items():
+#     print('\n' + k.upper())
+#     print(v.title())
+
+# friends = ['Jen', 'Cola', 'Java']
+# for name in favorite_language.values():
+#     print(name.title())
+#     for fr in friends:
+#         if fr in favorite_language:
+#             print('Hi ' + name.title() + ' I like to.')
+
+man = {
+    'first_name':'Jack',
+    'last_name':'Doe',
+    'age':'30',
+    'hair_color':'Black'
+}
+woman = {
+    'first_name':'Jena',
+    'last_name':'Jackson',
+    'age':'25',
+    'hair_color':'White'
+}
