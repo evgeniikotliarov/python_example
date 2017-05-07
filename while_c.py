9aefeb4976eabf73380232d7aa9cc2fde2351652
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-07 22:24:39 +0600

    Func input, string and numerabel

--- while_c.py
+++ while_c.py
@@ -16,3 +16,18 @@ if height <= 150:
     print('No')
 else:
     print("Yes")
+num = input("Enter your number ")
+num = int(num)
+
+if num % 2 == 0:
+    print("\n Your number good " + str(num) + " !")
+else:
+    print("\n No false " + str(num) + "not good")
+
+request = input("Сколько человек будет за столом?")
+request = int(request)
+if request >= 8:
+    print("За один стол " + str(request)+ " человек не поместяться")
+else:
+    print("Good " + str(request)+ " человек сядите")
+
