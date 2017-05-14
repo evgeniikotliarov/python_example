c9d8d065a36fa180139c5e881d3633444d683289
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-14 14:49:22 +0600

    example funtions

--- while_c.py
+++ while_c.py
@@ -86,24 +86,92 @@
 #     x+=1
 #     print(x)
 
-def wear(size, trademark):
-    print("This size "+ size +" I love python "+'and '+trademark.title())
+# def wear(size, trademark):
+#     print("This size "+ size +" I love python "+'and '+trademark.title())
+#
+# wear('l', 'adidas')
+# wear('M', 'nike')
+# wear(size = "Like", trademark = 'adidas')
+#
+#
+# def animal(name, type='dog'):
+#     print("This is "+name.title()+" is a "+type+" !")
+#
+# animal(name='charly')
+# animal('pussy', type='cat')
+#
+#
+# def describe_city(city, country='russia'):
+#     print(country.title()+" city is "+city.title())
+#
+# describe_city("Russia", 'moscow')
+# describe_city('kazan')
+# describe_city('ufa')
+#
+# def full_name(first_name, last_name, middle_name=''):
+#     formated_name = first_name.capitalize()+' '+last_name.capitalize()+' '+middle_name.title()
+#     return formated_name
+# print(full_name("edison", 'kovanio'))
+#
+# def build_person(first_name, last_name, age=''):
+#     person = {'first':first_name.title(), 'last':last_name.title()}
+#     if age:
+#         person['age'] = age
+#     return person
+#
+# musculian = build_person('vadim','antonov')
+# print(musculian)
+# musculian = build_person('misha', 'lotov', '25')
+# print(musculian)
 
-wear('l', 'adidas')
-wear('M', 'nike')
-wear(size = "Like", trademark = 'adidas')
-
-
-def animal(name, type='dog'):
-    print("This is "+name.title()+" is a "+type+" !")
-
-animal(name='charly')
-animal('pussy', type='cat')
-
-
-def describe_city(city, country='russia'):
-    print(country.title()+" city is "+city.title())
-
-describe_city("Russia", 'moscow')
-describe_city('kazan')
-describe_city('ufa')
+# def build_person(f_name, l_name, age=''):
+#     full_name = f_name+" "+l_name
+#     return full_name.title()
+#
+# while True:
+#     print("Please tell me your name")
+#     f_name = input("First name: ")
+#     if f_name == "q":
+#         break
+#     l_name = input("Enter your last name: ")
+#     if l_name == 'q':
+#         break
+#     formatted_name = build_person(f_name, l_name)
+#     print("Hello, " + formatted_name + "!")
+#
+#
+# def get_city(country, city, age=''):
+#     if age:
+#         city_builder = city+", "+country+', '+age
+#     else:
+#         city_builder = city+', ' + country+ ' !'
+#     return city_builder.title()
+#
+# love_city = get_city('Russia', 'Moscow', '855')
+# print(love_city)
+#
+# love_city = get_city('USA', 'Washington')
+# print(love_city)
+#
+#
+# def get_formatted_name(first_name, album, track=''):
+#     full_name = {'name':first_name.capitalize(), 'album': album.capitalize()}
+#     if track:
+#         full_name['track'] = track.capitalize()
+#     return full_name
+#
+# music = get_formatted_name('john bon jovi', 'my live', 'one')
+# print(music)
+# music = get_formatted_name('michael', 'jackson')
+# print(music)
+#
+# while True:
+#     print('\n Enter artist ')
+#     first_name = input("Name ")
+#     if first_name == "q":
+#         break
+#     album = input('Album ')
+#     if album == 'q':
+#         break
+#     artist = get_formatted_name(first_name, album)
+#     print(artist)
