19705f0f8c0aeb8caf7dc509f5887355be64f53a
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-05-17 15:37:01 +0600

    Two example

--- car/get_car.py
+++ car/get_car.py
@@ -1,7 +1,15 @@
-def make_car(mark, model, **other):
-    car = {}
-    car['mark'] = mark
-    car['model'] = model
-    for key, value in other.items():
-        car[key] = value
-    return car
+# def make_car(mark, model, **other):
+#     car = {}
+#     car['mark'] = mark
+#     car['model'] = model
+#     for key, value in other.items():
+#         car[key] = value
+#     return car
+
+def arifmetic(a, b=2017):
+    sum = b - a
+    return sum
+
+def full_name(first_name, last_name):
+    f_name = first_name.title() + last_name.title()
+    return f_name
--- practic/print_car.py
+++ practic/print_car.py
@@ -1,4 +1,22 @@
-from car.get_car import make_car
+# from car.get_car import make_car
+#
+# view_car = make_car('Toyota', 'Rav4', year='2017', color='silver')
+# print(view_car)
 
-view_car = make_car('Toyota', 'Rav4', year='2017', color='silver')
-print(view_car)
+# from car.get_car import arifmetic
+#
+# a = int(input("Enter first number "))
+# b = int(input("Enter second number "))
+# c = int(input("Enter third number "))
+#
+# arifmetic(a,b,c)
+from car.get_car import arifmetic, full_name
+name = input("Enter your name ")
+surname = input("Enter your surname ")
+year = int(input("When were your born "))
+country = input("Where are you from ")
+
+age = arifmetic(year)
+f_name = full_name(name, surname)
+
+print("Hello, "+f_name+". ""Your are "+str(age+" years old ." "You are living from "+country+" !")
