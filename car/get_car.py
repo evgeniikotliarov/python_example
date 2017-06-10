4bf0c1b328709cb74a09905f663dac21f649606d
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-10 23:49:08 +0600

    Классы

--- car/get_car.py
+++ car/get_car.py
@@ -1,16 +1,14 @@
-# def make_car(mark, model, **other):
-#     car = {}
-#     car['mark'] = mark
-#     car['model'] = model
-#     for key, value in other.items():
-#         car[key] = value
-#     return car
-#
-# def arifmetic(a, b=2017):
-#     sum = b - a
-#     return sum
-#
-# def full_name(first_name, last_name):
-#     f_name = first_name.title() + last_name.title()
-#     return f_name
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
 
+
+    def descriptive_name(self):
+        full_name = self.make +' '+ self.model + ' ' + str(self.year)
+        return full_name.title()
+
+
+my_new_car = Car('Toyota', 'camry', 2015)
+print(my_new_car.descriptive_name())
