8bc61a390f8980c7689e81a1c3a186c147e0bdb9
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-14 00:28:49 +0600

    Class super

--- car/get_car.py
+++ car/get_car.py
@@ -23,6 +23,33 @@ class Car:
         self.odometer_reading += miles
 
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+        def fill_gas_tank():
+            print("This car doesn't need a gas tank!")
+
+class Battery():
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery = Battery()
+
+        def __init__(self, battery_size=70):
+            self.battery_size = battery_size
+
+            def describe_battery(self):
+                print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+
+my_tesla = ElectricCar('Tesla', 'model', '2016')
+print(my_tesla.descriptive_name())
+my_tesla.describe_battery()
+
 my_new_car = Car('Toyota', 'camry', 2015)
 print(my_new_car.descriptive_name())
 my_new_car.odometer_reading = 100
