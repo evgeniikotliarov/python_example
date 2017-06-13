b8267eb3522fd7ff757a39d8886b6b8e55b2fb06
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-14 00:05:18 +0600

    Class car

--- car/get_car.py
+++ car/get_car.py
@@ -3,12 +3,30 @@ class Car:
         self.make = make
         self.model = model
         self.year = year
+        self.odometer_reading = 23
 
 
     def descriptive_name(self):
         full_name = self.make +' '+ self.model + ' ' + str(self.year)
         return full_name.title()
 
+    def reading_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("False")
+
+    def increment_odometer(self, miles):
+        self.odometer_reading += miles
+
 
 my_new_car = Car('Toyota', 'camry', 2015)
 print(my_new_car.descriptive_name())
+my_new_car.odometer_reading = 100
+my_new_car.update_odometer(222)
+my_new_car.increment_odometer(15000)
+my_new_car.reading_odometer()
+
