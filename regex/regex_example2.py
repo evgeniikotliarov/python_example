3c38e6ac74103538686aa931027276f704e56978
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-15 10:37:10 +0600

    Norm.mask

--- regex/regex_example2.py
+++ regex/regex_example2.py
@@ -1,8 +1,10 @@
 import re
 
-number_mapping = {'1': 'one',
-                  '2': 'two',
+number_mapping = {'\d': '[0-9]',
+                  '\w': '[a-zA-Z]',
                   '3': 'three'}
-s = "1 testing 2 3"
-
-print( re.sub(r'\d', lambda x: number_mapping[x.group()], s))
+s = "\d testing \w 3"
+for k, v in number_mapping.items():
+    s = s.replace(k, v)
+print(s)
+# print( re.sub(r'\d', lambda x: number_mapping[x.group()], s))
