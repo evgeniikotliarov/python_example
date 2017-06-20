65cd59eebfc78ee4cd5a3eb64c99137502a582d0
Evgenii Kotliarov <kotlyarov.ev@gmail.com>
2017-06-20 12:23:38 +0600

    class Car and ...

--- car/get_car.py
+++ car/get_car.py
@@ -33,3 +33,22 @@ my_used_car.read_odometer()
 my_used_car.increment_odometer(5505)
 my_used_car.read_odometer()
 
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+        self.battery_size = 70
+        self.battery = Battery()
+
+    def fill_gas_tank(self):
+        print("This car doesn't need a gas tank!")
+
+class Battery():
+    def __init__(self, battery_size=40):
+        self.battery_size = battery_size
+
+
+    def describe_battery(self):
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+my_tesla = ElectricCar('tesla', "model's", 2016)
+my_tesla.battery.describe_battery()
